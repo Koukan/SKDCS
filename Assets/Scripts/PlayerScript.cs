5d90a1882271592efb50a7208687b4291ccfa129
Mickael Cerisier <miccerisier@gmail.com>
2014-05-03 16:36:26 +0200

    Wall Jump Feature

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,8 +5,11 @@ public class PlayerScript : MonoBehaviour
 {
     private Animator anim;
 	public Vector2	speed = new Vector2(50, 50);
+    public LayerMask mask;
+    public float deltaTimeJump;
 	private Vector2 movement;
-	private bool	grounded = false;
+	private bool grounded = false;
+    private float timeJump = 0;
 	
     void Start()
     {
@@ -18,8 +21,13 @@ public class PlayerScript : MonoBehaviour
 	void Update () {
 		float inputX = Input.GetAxis("Horizontal");
 
-		if (inputX != 0)
-			rigidbody2D.AddForce(new Vector2(speed.x * inputX, 0.1f));
+        if (inputX != 0)
+        {
+            if (grounded)
+                rigidbody2D.AddForce(new Vector2(speed.x * inputX, 0.1f));
+            else
+                rigidbody2D.AddForce(new Vector2(0.15f * speed.x * inputX, 0.1f));
+        }
 
         anim.SetFloat("Speed", Mathf.Abs(rigidbody2D.velocity.x));
 		
@@ -34,27 +42,57 @@ public class PlayerScript : MonoBehaviour
 			}
 		}
 
-		if (grounded && Input.GetButtonDown("Jump"))
+        if (!grounded)
+            timeJump += Time.deltaTime;
+
+		if (Input.GetButtonDown("Jump"))
 		{
-			rigidbody2D.AddForce(new Vector2(0, speed.y));
-			grounded = false;
+            if (grounded && timeJump > deltaTimeJump)
+            {
+                rigidbody2D.AddForce(new Vector2(0, speed.y));
+                grounded = false;
+                timeJump = 0;
+            }
+            else if (Physics2D.Raycast(transform.position, new Vector2(1, -0.5f), 1.2f, mask))
+            {
+                rigidbody2D.velocity = new Vector2(0, 0);
+                rigidbody2D.AddForce(new Vector2(-2000, speed.y));
+            }
+            else if (Physics2D.Raycast(transform.position, new Vector2(-1, -0.5f), 1.2f, mask))
+            {
+                rigidbody2D.velocity = new Vector2(0, 0);
+                rigidbody2D.AddForce(new Vector2(2000, speed.y));
+            }
 		}
 
 
 	}
 
-	void OnCollisionEnter2D(Collision2D col) 
+	/*void OnCollisionEnter2D(Collision2D col) 
 	{
 		if (col.gameObject.layer == LayerMask.NameToLayer("Ground"))
 			grounded = true;
-	}
+	}*/
 
 	void FixedUpdate()
 	{
+        if (!grounded && timeJump > deltaTimeJump)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(0, -1), 0.82f, mask);
+            if (hit.collider != null)
+                grounded = true;
+        }
+
 		Vector2 v = rigidbody2D.velocity;
 		rigidbody2D.velocity = new Vector2(Mathf.Clamp(v.x, -5, 5), v.y);
 
 		if (v.x < -0.1f && transform.localScale.x < 0 || v.x > 0.1f && transform.localScale.x > 0)
 			transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
 	}
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(1, -1, 0));
+    }
 }
