ac40b63a7e82e9cba6a4fecbefa173cee759f7df
Mickael Cerisier <miccerisier@gmail.com>
2014-05-03 19:20:55 +0200

    Improve gameplay

---
+++ Assets/Scripts/PlayerNotifier.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerNotifier : MonoBehaviour
+{
+    public GameObject player;
+    public string message;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        player.SendMessage("Message" + message + "Enter", other, SendMessageOptions.DontRequireReceiver);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        player.SendMessage("Message" + message + "Exit", other, SendMessageOptions.DontRequireReceiver);
+    }
+}
