659cb85f3e02b3e4c07ebd86edb239cf698c2cdb
Mickael Cerisier <miccerisier@gmail.com>
2014-05-03 19:52:24 +0200

    Improve wallJump

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,17 +7,20 @@ public class PlayerScript : MonoBehaviour
 	public Vector2	speed = new Vector2(50, 50);
     public LayerMask mask;
     public float deltaTimeJump;
+    public float wallJumpForce;
 	private Vector2 movement;
-	private bool grounded = false;
+	private bool grounded = true;
     private int walled = 0;
     private float timeJump = 0;
     private Vector3 startposition;
     private Vector2 hitbox;
+    private float jumpActivated = 0;
 
     void Start()
     {
         anim = GetComponent<Animator>();
         transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        timeJump = Mathf.Infinity;
         GameEventManager.GameStart += GameStart;
         GameEventManager.GameOver += GameOver;
         enabled = false;
@@ -37,45 +40,33 @@ public class PlayerScript : MonoBehaviour
                 rigidbody2D.AddForce(new Vector2(0.15f * speed.x * inputX, 0.1f));
         }
 
+        // Animation Variable
         anim.SetFloat("Speed", Mathf.Abs(rigidbody2D.velocity.x));
-		
-		// 5 - Shooting
-		/*if (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Fire2"))
-		{
-			WeaponScript weapon = GetComponent<WeaponScript>();
-			if (weapon != null)
-			{
-				// false because the player is not an enemy
-				weapon.Attack(false);
-			}
-		}*/
 
         if (!grounded)
             timeJump += Time.deltaTime;
 
-		if (Input.GetButtonDown("Jump"))
+		if (Input.GetButtonDown("Jump") || jumpActivated > 0)
 		{
             if (grounded && timeJump > deltaTimeJump)
             {
+                rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0);
                 rigidbody2D.AddForce(new Vector2(0, speed.y));
                 timeJump = 0;
+                jumpActivated = 0;
             }
-            else if (walled != 0)
+            else if (walled != 0 && wallJumpForce > 0)
             {
                 rigidbody2D.velocity = new Vector2(0, 0);
-                rigidbody2D.AddForce(new Vector2(2000 * -walled, speed.y));
-                /*RaycastHit2D hit;
-                if ((hit = Physics2D.Raycast(transform.position, new Vector2(1, 0.5f).normalized, hitbox.x + 0.2f, mask)) && hit.collider)
-                {
-                    rigidbody2D.velocity = new Vector2(0, 0);
-                    rigidbody2D.AddForce(new Vector2(-2000, speed.y));
-                }
-                else if ((hit = Physics2D.Raycast(transform.position, new Vector2(-1, -0.5f).normalized, hitbox.x + 0.2f, mask)) && hit.collider)
-                {
-                    rigidbody2D.velocity = new Vector2(0, 0);
-                    rigidbody2D.AddForce(new Vector2(2000, speed.y));
-                }*/
+                rigidbody2D.AddForce(new Vector2(wallJumpForce * -walled, speed.y));
+                jumpActivated = 0;
             }
+            else if (jumpActivated > 0.1)
+                jumpActivated = 0;
+            else if (jumpActivated > 0)
+                jumpActivated += Time.deltaTime;
+            else
+                jumpActivated = Time.deltaTime;
 		}
         if (transform.position.y < -20)
             GameEventManager.TriggerGameOver();
