7c630e20157bb4899aa97eb11f76bf39685366fa
Vivien Botton <vbotton@gmail.com>
2014-05-04 11:07:35 +0200

    fix sound and gameover

--- Assets/Scripts/GuiManager.cs
+++ Assets/Scripts/GuiManager.cs
@@ -3,19 +3,29 @@ using System.Collections;
 
 public class GuiManager : MonoBehaviour {
     public GUIText instructionsText;
+    public GUIText gameoverText;
+    private static bool _gameover = false;
 	// Use this for initialization
 	void Start ()
     {
+        gameoverText.enabled = _gameover;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetButtonDown("Jump"))
         {
+            gameoverText.enabled = false;
             instructionsText.enabled = false;
             GameEventManager.TriggerGameStart();
             enabled = false;
+            _gameover = true;
             Destroy(gameObject);
         }
 	}
-}
+
+    public void GameOver() {
+        gameoverText.enabled = true;
+    }
+
+ }
